59f9c762846ac7f87643830e1b01344cd3ff1413
SamuelERnandes <samuelernandes_2012@hotmail.com>
2024-10-23 19:27:25 -0300

    subindo primeira aula do professor Ivan

---
+++ src/main/java/com/example/movies/model/Movie.java
@@ -0,0 +1,45 @@
+package com.example.movies.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(id, movie.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/resources/db/migration/V1__create-table-movie.sql
@@ -0,0 +1,4 @@
+create table movies (
+    id int not null primary key auto_increment,
+    nome varchar(200)
+);
